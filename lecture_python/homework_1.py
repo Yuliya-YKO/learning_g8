678144f30467d31505341f465bc2e067587e248f
Centr <centr@MacBook-Air-Centr.local>
2023-07-26 21:59:15 +0300

    first homework

---
+++ lecture_python/homework_1.py
@@ -0,0 +1,13 @@
+# task 1
+list_of_numbers = [1,3,5,7]
+print(sum(list_of_numbers))
+
+# task 2
+list_of_numbers2 = [1,3,3,5,7,9,9,12,15]
+list_of_unique_numbers2 = list(set(list_of_numbers2))
+print(sum(list_of_unique_numbers2))
+
+# task 3
+dct = {'salary': 100}
+dct['salary'] = dct['salary']*1.5
+print(dct['salary'])
